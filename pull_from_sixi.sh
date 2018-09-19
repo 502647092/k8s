155293174eb5405df2092f5d333ebc5e5aabc6e2
MiaoWoo <admin@yumc.pw>
2018-09-19 09:44:52 +0800

    Update pull_from_sixi.sh

--- pull_from_sixi.sh
+++ pull_from_sixi.sh
@@ -1,4 +1,4 @@
-SOURCE='docker.sixi.com/miaowoo'
+SOURCE='registry.sixi.com/miaowoo'
 TARGET='k8s.gcr.io'
 VERSION='v1.11.3'
 SERVER="kubernetes-dashboard-amd64:v1.10.0 kube-apiserver-amd64:${VERSION} kube-controller-manager-amd64:${VERSION} kube-scheduler-amd64:${VERSION} kube-proxy-amd64:${VERSION} pause:3.1 etcd-amd64:3.2.18 coredns:1.1.3"
