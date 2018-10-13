c1c67823de0a3b041b93a0034f23c5233c506cc9
MiaoWoo <admin@yumc.pw>
2018-10-13 11:18:56 +0800

    Update pull_from_sixi.sh

--- pull_from_sixi.sh
+++ pull_from_sixi.sh
@@ -6,23 +6,23 @@ K8S_VERSION='v1.12.1'
 docker login ${SOURCE} -u ${USERNAME} -p ${PASSWORD}
 
 PRIVATE_IMAGES=(
-k8s.gcr.io/kube-apiserver:${K8S_VERSION}"
-k8s.gcr.io/kube-controller-manager:${K8S_VERSION}"
-k8s.gcr.io/kube-scheduler:${K8S_VERSION}"
-k8s.gcr.io/kube-proxy:${K8S_VERSION}"
-k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.0"
-k8s.gcr.io/metrics-server-amd64:v0.3.1"
-k8s.gcr.io/heapster-amd64:v1.5.4"
-k8s.gcr.io/pause:3.1"
-k8s.gcr.io/etcd:3.2.24"
-k8s.gcr.io/coredns:1.2.2"
+k8s.gcr.io/kube-apiserver:${K8S_VERSION}
+k8s.gcr.io/kube-controller-manager:${K8S_VERSION}
+k8s.gcr.io/kube-scheduler:${K8S_VERSION}
+k8s.gcr.io/kube-proxy:${K8S_VERSION}
+k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.0
+k8s.gcr.io/metrics-server-amd64:v0.3.1
+k8s.gcr.io/heapster-amd64:v1.5.4
+k8s.gcr.io/pause:3.1
+k8s.gcr.io/etcd:3.2.24
+k8s.gcr.io/coredns:1.2.2
 gcr.io/kubernetes-helm/tiller:v2.11.0
 )
 
 IMAGES=(
-weaveworks/weave-kube:2.4.1"
-weaveworks/weave-npc:2.4.1"
-rook/ceph:master"
+weaveworks/weave-kube:2.4.1
+weaveworks/weave-npc:2.4.1
+rook/ceph:master
 )
 
 echo "Pull and Tag Private Images..."
